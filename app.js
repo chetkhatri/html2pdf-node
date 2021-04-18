db537e443f0de5e8c2ff2edaf64248ff68a044dc
Chetan Khatri <chetan.khatri@accionlabs.com>
2021-04-18 13:36:40 -0400

    Server side validation added

--- app.js
+++ app.js
@@ -9,14 +9,32 @@ var wkhtmltopdf = require('wkhtmltopdf');
 app.set('view engine','pug')
 
 const bodyParser = require('body-parser')
+const {check, validationResult} = require('express-validator');
+
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
   res.render('index')
 })
 
-app.post('/submit', async function (req, res, next)  {
+app.post('/submit', [
+    check('custName').trim().escape().not().isEmpty().withMessage('First Name can not be empty!')
+    .bail().isAlpha().isLength({ min: 4, max: 15 })
+      .withMessage('Minimum 4 Characters and Maximum 15 Characters are needed!').bail(),
+    check('custEmail', 'Email Can not be empty').not().isEmpty().isEmail()
+    .normalizeEmail().withMessage('Email should be in right format!').bail(),
+    check('subject', 'Subject can not be empty').not().isEmpty(),
+    check('telnumber', 'Telephone number can not be empty').not().isEmpty()
+      .isNumeric().withMessage('Telephone number must be numberic').bail(),
+    check('question', 'Question can not be empty').not().isEmpty(),
+], function (req, res)  {
 
+  const errors = validationResult(req);
+  console.log(req.body);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).jsonp(errors.array());
+  } else {
     var pdfValues = 
 
 `<table style="width:100%">
@@ -40,10 +58,13 @@ app.post('/submit', async function (req, res, next)  {
   res.writeHead(200, {'Content-Type': 'application/pdf'});
   wkhtmltopdf(pdfValues).pipe(res);
 
+  }
+
+    
 
 })
 
 app.listen(3232)
 process.on('uncaughtException', function (err) {
-  console.log('Caught exception: ', err);
+  console.log('Caught exception: ', err); 
 });
