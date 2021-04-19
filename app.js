ac9ddaec306fd0e33010c9dccdd916ae1a8c86ba
Chetan Khatri <chetan.khatri@accionlabs.com>
2021-04-19 04:38:44 -0400

    URL encoding added

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express()
 const process = require('process');
 var fs = require('fs');
 var url = require('url');
+var encodeUrl = require('encodeurl')
+var escapeHtml = require('escape-html')
 
 var wkhtmltopdf = require('wkhtmltopdf');
 
@@ -23,18 +25,23 @@ app.post('/submit', [
       .withMessage('Minimum 4 Characters and Maximum 15 Characters are needed!').bail(),
     check('custEmail', 'Email Can not be empty').not().isEmpty().isEmail()
     .normalizeEmail().withMessage('Email should be in right format!').bail(),
-    check('subject', 'Subject can not be empty').not().isEmpty(),
+    check('subject', 'Invalid value!').not().isEmpty().matches("^(?!.*<[^>]+>).*"),
     check('telnumber', 'Telephone number can not be empty').not().isEmpty()
       .isNumeric().withMessage('Telephone number must be numberic').bail(),
-    check('question', 'Question can not be empty').not().isEmpty(),
+    check('question', 'Question can not be empty').not().isEmpty().isAlpha()
+      .withMessage('Only Alphanumberic with space allowed in question').bail(),
 ], function (req, res)  {
 
   const errors = validationResult(req);
   console.log(req.body);
+  
+  var encodedSubject = encodeUrl(req.body.subject)
 
   if (!errors.isEmpty()) {
     return res.status(422).jsonp(errors.array());
   } else {
+    var subjectValue = decodeURIComponent(decodeURIComponent(req.body.subject))
+    
     var pdfValues = 
 
 `<table style="width:100%">
@@ -49,7 +56,7 @@ app.post('/submit', [
     <td>${req.body.custName}</td>
     <td>${req.body.custEmail}</td>
     <td>${req.body.telnumber}</td>
-    <td>${req.body.subject}</td>
+    <td>${subjectValue}</td>
     <td>${req.body.question}</td>
   </tr>
 </table>`
